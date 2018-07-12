c413e91aa41993b62146e1edf2c8c242372a01c1
YouerSu <youersu@outlook.com>
2018-07-12 16:52:48 +0800

    fix bug

--- app/src/main/java/com/example/administrator/buildings/StoresEmployee.java
+++ app/src/main/java/com/example/administrator/buildings/StoresEmployee.java
@@ -1,11 +1,12 @@
 package com.example.administrator.buildings;
 
 
+
 public class StoresEmployee extends Employee{
 
     @Override
     public boolean work(Item item) {
-        if (item instanceof SellItem) return false;
+        if (!(item instanceof SellItem)) return false;
         item.setTotal(item.getTotal()-1);
         for (Character character:findMaster(Building.findWorkSpace(getWorkSpace()).getMaster(),characters))
             if (character!=null)
