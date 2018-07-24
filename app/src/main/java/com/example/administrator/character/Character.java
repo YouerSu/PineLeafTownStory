85b352559352aef565522cf668395d735633471b
YouerSu <39619473+YouerSu@users.noreply.github.com>
2018-07-24 22:17:51 +0800

    Update Character.java

--- app/src/main/java/com/example/administrator/character/Character.java
+++ app/src/main/java/com/example/administrator/character/Character.java
@@ -24,36 +24,41 @@ public abstract class Character implements OwnName,ShowAdapter{
     private int salary;
     private String workSpace = "PineTower";
 
+     abstract void initialization();
+
     @Override
     public Map<String, String> UIPageAdapter() {
         return GameUI.getAdapterMap(name,"薪水："+salary,"工作地点："+workSpace,"职业："+getClass().getName().substring(getClass().getName().lastIndexOf(".")+1));
     }
-
-    abstract void initialization();
 
     @Override
     public void onClick(GameUI gameUI) {
         gameUI.dialogueBox(name+":你好,"+Player.getPlayerName());
     }
 
-
     public static void getAllDate(){
         Building.getBuildingDate();
         getDate(Sql.getDateBase().rawQuery("select * from "+Info.CHARACTER,null));
     }
 
-    public static void createNewCharacter(SQLiteDatabase db, String className, String name, int money, int prestige, int x_coordinate, int salary, String workSpace){
-        if (!(Tools.getType(className) instanceof Character)) return;
-        db.execSQL
-        ("insert into "+Info.CHARACTER+" ("+Info.id+","+Info.NAME+","+Info.MONEY+","+Info.PRESTIGE+","+Info.coordinate+","+Info.salary+","+Info.MASTER+") values ('"+className+"','"+name+"',"+money+","+prestige+","+x_coordinate+","+salary+",'"+workSpace+"')");
-        Character character = Tools.getType(className);
-        character.setName(name);
-        character.setMoney(money);
-        character.setPrestige(prestige);
-        character.setX_coordinate(x_coordinate);
-        character.setSalary(salary);
-        character.setWorkSpace(workSpace);
-        getCharacters().add(character);
+    public static void getDate(Cursor iDate) {
+        while (iDate.moveToNext()){
+            Character character = Tools.getType(iDate.getString(iDate.getColumnIndex(Info.id)));
+            character.setName(iDate.getString(iDate.getColumnIndex(Info.NAME)));
+            character.setMoney(iDate.getInt(iDate.getColumnIndex(Info.MONEY)));
+            character.setPrestige(iDate.getInt(iDate.getColumnIndex(Info.PRESTIGE)));
+            character.setX_coordinate(iDate.getInt(iDate.getColumnIndex(Info.coordinate)));
+            character.setWorkSpace(iDate.getString(iDate.getColumnIndex(Info.MASTER)));
+            character.setSalary(iDate.getInt(iDate.getColumnIndex(Info.salary)));
+            character.initialization();
+            characters.add(character);
+        }
+        iDate.close();
+    }
+
+    public static void saveAllDate() {
+        saveBuildingDate();
+        saveCharacterDate();
     }
 
     public static void saveBuildingDate(){
@@ -68,31 +73,24 @@ public abstract class Character implements OwnName,ShowAdapter{
             character.saveDate();
     }
 
-    public static void saveAllDate() {
-        saveBuildingDate();
-        saveCharacterDate();
-    }
-
-
     public void saveDate(){
         Sql.operatingSql(new String[]{
         "update "+Info.CHARACTER+" set "+Info.id+" = '"+getClass().getName()+"',"+Info.MONEY+" = "+money+","+Info.PRESTIGE+" = "+prestige+","+Info.coordinate+" = "+x_coordinate+","+Info.salary+" = "+salary+","+Info.MASTER+" = '"+ workSpace +"' where "+Info.NAME+" = '"+ name +"'"
         });
     }
 
-    public static void getDate(Cursor iDate) {
-        while (iDate.moveToNext()){
-        Character character = Tools.getType(iDate.getString(iDate.getColumnIndex(Info.id)));
-        character.setName(iDate.getString(iDate.getColumnIndex(Info.NAME)));
-        character.setMoney(iDate.getInt(iDate.getColumnIndex(Info.MONEY)));
-        character.setPrestige(iDate.getInt(iDate.getColumnIndex(Info.PRESTIGE)));
-        character.setX_coordinate(iDate.getInt(iDate.getColumnIndex(Info.coordinate)));
-        character.setWorkSpace(iDate.getString(iDate.getColumnIndex(Info.MASTER)));
-        character.setSalary(iDate.getInt(iDate.getColumnIndex(Info.salary)));
-        character.initialization();
-        characters.add(character);
-        }
-        iDate.close();
+    public static void createNewCharacter(SQLiteDatabase db, String className, String name, int money, int prestige, int x_coordinate, int salary, String workSpace){
+        if (!(Tools.getType(className) instanceof Character)) return;
+        db.execSQL
+        ("insert into "+Info.CHARACTER+" ("+Info.id+","+Info.NAME+","+Info.MONEY+","+Info.PRESTIGE+","+Info.coordinate+","+Info.salary+","+Info.MASTER+") values ('"+className+"','"+name+"',"+money+","+prestige+","+x_coordinate+","+salary+",'"+workSpace+"')");
+        Character character = Tools.getType(className);
+        character.setName(name);
+        character.setMoney(money);
+        character.setPrestige(prestige);
+        character.setX_coordinate(x_coordinate);
+        character.setSalary(salary);
+        character.setWorkSpace(workSpace);
+        getCharacters().add(character);
     }
 
     public void wages(){
